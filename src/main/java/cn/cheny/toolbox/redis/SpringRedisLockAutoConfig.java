0059789c099c9ad6960b55e8b211c9c2066e7a37
cheney <478241123@qq.com>
2020-08-26 20:58:16 +0800

    update SpringRedisLockAutoConfig

--- src/main/java/cn/cheny/toolbox/redis/SpringRedisLockAutoConfig.java
+++ src/main/java/cn/cheny/toolbox/redis/SpringRedisLockAutoConfig.java
@@ -14,7 +14,6 @@ import cn.cheny.toolbox.spring.SpringToolAutoConfig;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
