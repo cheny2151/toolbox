0fded12ae0d71480f0f88637183d09f43da2e160
cheny <478241123@qq.com>
2020-08-27 15:18:52 +0800

    update 修改使用的工具类

--- src/main/java/cn/cheny/toolbox/reflect/methodHolder/MethodHolder.java
+++ src/main/java/cn/cheny/toolbox/reflect/methodHolder/MethodHolder.java
@@ -1,5 +1,7 @@
 package cn.cheny.toolbox.reflect.methodHolder;
 
+import cn.cheny.toolbox.reflect.methodHolder.exception.FindNotUniqueMethodException;
+
 import java.lang.reflect.Method;
 import java.util.Optional;
 
@@ -10,21 +12,6 @@ import java.util.Optional;
  * @date 2019-12-05
  */
 public interface MethodHolder {
-
-    /**
-     * 缓存方法
-     *
-     * @param method 方法
-     */
-    void cacheMethod(Method method);
-
-    /**
-     * 缓存方法
-     *
-     * @param methodKey 方法key(默认为方法名)
-     * @param method    方法
-     */
-    void cacheMethod(String methodKey, Method method);
 
     /**
      * 反射调用方法
@@ -75,21 +62,21 @@ public interface MethodHolder {
 
     /**
      * 通过方法key(默认为方法名)获取方法
+     * 查询多个方法时抛出该异常:{@link FindNotUniqueMethodException}
      *
      * @param methodKey 方法key(默认为方法名)
      * @return 方法
-     * @throws reflect.methodHolder.exception.FindNotUniqueMethodException 查询多个方法时抛出该异常
      */
     Optional<Method> getMethod(String methodKey);
 
     /**
      * 通过给定方法key(默认为方法名)和其他参数推测方法
+     * 查询多个方法时抛出该异常:{@link FindNotUniqueMethodException}
      *
      * @param methodKey  方法key(默认为方法名)，必填
      * @param returnType 返回类型，可为空
      * @param args       参数类型数组，可为空
      * @return 方法
-     * @throws reflect.methodHolder.exception.FindNotUniqueMethodException 查询多个方法时抛出该异常
      */
     Optional<Method> speculateMethod(String methodKey, Class<?> returnType, Class<?>... args);
 
