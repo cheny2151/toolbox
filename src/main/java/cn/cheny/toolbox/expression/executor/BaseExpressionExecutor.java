ed47dd28c2334801a09e40078a7685993497aba9
cheny <478241123@qq.com>
2020-09-04 11:45:20 +0800

    update 表达式新增指向环境变量的变量名

--- src/main/java/cn/cheny/toolbox/expression/executor/BaseExpressionExecutor.java
+++ src/main/java/cn/cheny/toolbox/expression/executor/BaseExpressionExecutor.java
@@ -22,9 +22,14 @@ import static cn.cheny.toolbox.expression.CharConstants.*;
 public abstract class BaseExpressionExecutor implements ExpressionExecutor {
 
     /**
+     * 指向环境值变量名
+     */
+    public final static String ENV_REF = "$env";
+
+    /**
      * 系统创建的环境值key
      */
-    public static final String NEW_ENV_KEY = "_ENV_KEY";
+    private static final String NEW_ENV_KEY = "_ENV_KEY";
 
     /**
      * 表达式
@@ -92,6 +97,8 @@ public abstract class BaseExpressionExecutor implements ExpressionExecutor {
                 } else if (CONTAINS_OPERATOR_PATTERN.matcher(valueStr).find()) {
                     // 结合Aviator,将含运算符的arg丢给Aviator执行
                     return executeOperation(valueStr, env, true);
+                } else if (ENV_REF.equals(valueStr)) {
+                    return env;
                 }
                 return castToBasic(valueStr);
             }
