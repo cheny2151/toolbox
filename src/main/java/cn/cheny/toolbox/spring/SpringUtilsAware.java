91dd24102dcdfd2d9779c7b48cfe13e02d2be0bb
chenyi5 <chenyi5@yy.com>
2021-06-25 16:32:33 +0800

    优化SpringUtilsAware

--- src/main/java/cn/cheny/toolbox/spring/SpringUtilsAware.java
+++ src/main/java/cn/cheny/toolbox/spring/SpringUtilsAware.java
@@ -1,13 +1,20 @@
 package cn.cheny.toolbox.spring;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * spring utils 创建初始化后执行
  *
- * @date 2021/4/21
  * @author by chenyi
+ * @date 2021/4/21
  */
 public interface SpringUtilsAware {
 
     void after();
+
+    static List<SpringUtilsAware> defaultAware() {
+        return Arrays.asList(new StaticPropertySpringUtils());
+    }
 
 }
