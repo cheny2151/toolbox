0c0f9ecdc15329cfbd0dc93753f8495ab48c26c9
chenyi5 <chenyi5@yy.com>
2021-07-27 20:36:48 +0800

    优化Parallel子类

--- src/main/java/cn/cheny/toolbox/asyncTask/parallel/OneParallel.java
+++ src/main/java/cn/cheny/toolbox/asyncTask/parallel/OneParallel.java
@@ -2,6 +2,7 @@ package cn.cheny.toolbox.asyncTask.parallel;
 
 import cn.cheny.toolbox.asyncTask.function.OneTaskConsume;
 import cn.cheny.toolbox.asyncTask.function.Task;
+import cn.cheny.toolbox.exception.ToolboxRuntimeException;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -19,10 +20,16 @@ public class OneParallel<ONE, RESULT> implements Parallel<RESULT> {
 
     private OneTaskConsume<ONE, RESULT> consume;
 
-    private ExecutorService executor;
+    private final ExecutorService executor;
+
+    private FutureResultHolder<ONE> oneResult;
+
+    private OneParallel(ExecutorService executor) {
+        this.executor = executor == null ? Executors.newSingleThreadExecutor() : executor;
+    }
 
     public OneParallel(Task<ONE> task, OneTaskConsume<ONE, RESULT> consume) {
-        this(task,consume, Executors.newSingleThreadExecutor());
+        this(task, consume, Executors.newSingleThreadExecutor());
     }
 
     public OneParallel(Task<ONE> task, OneTaskConsume<ONE, RESULT> consume, ExecutorService executor) {
@@ -31,10 +38,37 @@ public class OneParallel<ONE, RESULT> implements Parallel<RESULT> {
         this.executor = executor;
     }
 
+    public static <ONE, RESULT> OneParallel<ONE, RESULT> build() {
+        return build(null);
+    }
+
+    public static <ONE, RESULT> OneParallel<ONE, RESULT> build(ExecutorService executor) {
+        return new OneParallel<>(executor);
+    }
+
+    public void subOneTask(Task<ONE> task) {
+        this.one = task;
+        Future<ONE> submit = executor.submit(task::doTask);
+        this.oneResult = new FutureResultHolder<>(submit);
+    }
+
+    public RESULT consume(OneTaskConsume<ONE, RESULT> consume) {
+        if (this.oneResult == null) {
+            throw new ToolboxRuntimeException("undone one task");
+        }
+        this.consume = consume;
+        return consume.consume(oneResult);
+    }
+
     @Override
     public RESULT start() {
-        Future<ONE> oneFuture = executor.submit(() -> one.doTask());
-        FutureResultHolder<ONE> resultHolder = new FutureResultHolder<>(oneFuture);
-        return consume.consume(resultHolder);
+        if (executor == null || consume == null) {
+            throw new IllegalArgumentException();
+        }
+        FutureResultHolder<ONE> oneResult = this.oneResult;
+        if (oneResult == null && one != null) {
+            this.subOneTask(one);
+        }
+        return consume(consume);
     }
 }
--- src/main/java/cn/cheny/toolbox/asyncTask/parallel/TwoParallel.java
+++ src/main/java/cn/cheny/toolbox/asyncTask/parallel/TwoParallel.java
@@ -2,6 +2,7 @@ package cn.cheny.toolbox.asyncTask.parallel;
 
 import cn.cheny.toolbox.asyncTask.function.Task;
 import cn.cheny.toolbox.asyncTask.function.TwoTaskConsume;
+import cn.cheny.toolbox.exception.ToolboxRuntimeException;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -16,11 +17,20 @@ import java.util.concurrent.Future;
 public class TwoParallel<ONE, TWO, RESULT> implements Parallel<RESULT> {
 
     private Task<ONE> one;
+
     private Task<TWO> two;
 
     private TwoTaskConsume<ONE, TWO, RESULT> consume;
 
-    private ExecutorService executor;
+    private final ExecutorService executor;
+
+    private FutureResultHolder<ONE> oneResult;
+
+    private FutureResultHolder<TWO> twoResult;
+
+    public TwoParallel(ExecutorService executor) {
+        this.executor = executor == null ? Executors.newFixedThreadPool(2) : executor;
+    }
 
     public TwoParallel(Task<ONE> task1, Task<TWO> task2, TwoTaskConsume<ONE, TWO, RESULT> consume) {
         this(task1, task2, consume, Executors.newFixedThreadPool(2));
@@ -33,12 +43,47 @@ public class TwoParallel<ONE, TWO, RESULT> implements Parallel<RESULT> {
         this.executor = executor;
     }
 
+    public static <ONE, TWO, RESULT> TwoParallel<ONE, TWO, RESULT> build() {
+        return build(null);
+    }
+
+    public static <ONE, TWO, RESULT> TwoParallel<ONE, TWO, RESULT> build(ExecutorService executor) {
+        return new TwoParallel<>(executor);
+    }
+
+    public void subOneTask(Task<ONE> task) {
+        this.one = task;
+        Future<ONE> submit = executor.submit(task::doTask);
+        this.oneResult = new FutureResultHolder<>(submit);
+    }
+
+    public void subTwoTask(Task<TWO> task) {
+        this.two = task;
+        Future<TWO> submit = executor.submit(task::doTask);
+        this.twoResult = new FutureResultHolder<>(submit);
+    }
+
+    public RESULT consume(TwoTaskConsume<ONE, TWO, RESULT> consume) {
+        if (this.oneResult == null || this.twoResult == null) {
+            throw new ToolboxRuntimeException("undone one task");
+        }
+        this.consume = consume;
+        return consume.consume(oneResult, twoResult);
+    }
+
     @Override
     public RESULT start() {
-        Future<ONE> oneFuture = executor.submit(() -> one.doTask());
-        Future<TWO> twoFuture = executor.submit(() -> two.doTask());
-        FutureResultHolder<ONE> result1Holder = new FutureResultHolder<>(oneFuture);
-        FutureResultHolder<TWO> result2Holder = new FutureResultHolder<>(twoFuture);
-        return consume.consume(result1Holder, result2Holder);
+        if (executor == null || consume == null) {
+            throw new IllegalArgumentException();
+        }
+        FutureResultHolder<ONE> oneResult = this.oneResult;
+        if (oneResult == null && one != null) {
+            this.subOneTask(one);
+        }
+        FutureResultHolder<TWO> twoResult = this.twoResult;
+        if (twoResult == null && two != null) {
+            this.subTwoTask(two);
+        }
+        return consume(consume);
     }
 }
