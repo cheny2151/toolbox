733cec490387b1a24f11665b86b521e6873a83fc
chenyi5 <chenyi5@joyy.com>
2021-11-15 20:39:26 +0800

    update RedisCoordinatorConstant

--- src/main/java/cn/cheny/toolbox/coordinator/redis/RedisCoordinatorConstant.java
+++ src/main/java/cn/cheny/toolbox/coordinator/redis/RedisCoordinatorConstant.java
@@ -1,15 +1,17 @@
 package cn.cheny.toolbox.coordinator.redis;
 
 /**
+ * redis协调器常量/脚本
+ *
  * @author by chenyi
  * @date 2021/11/12
  */
 public class RedisCoordinatorConstant {
 
-    public static final String REDIS_CHANNEL = "COORDINATOR:RE_BALANCE:CHANNEL";
-    public static final String RESOURCES_REGISTER = "COORDINATOR:REGISTER";
+    public static final String REDIS_CHANNEL = "{COORDINATOR}:RE_BALANCE:CHANNEL";
+    public static final String RESOURCES_REGISTER = "{COORDINATOR}:REGISTER";
     public static final String RE_BALANCE_LOCK = "COORDINATOR:RE_BALANCE:LOCK";
-    public static final String HEART_BEAT_KEY_PRE = "COORDINATOR:HEART_BEAT";
+    public static final String HEART_BEAT_KEY_PRE = "{COORDINATOR}:HEART_BEAT";
     public static final String HEARTBEAT_VAL = "1";
 
     public static final String INIT_REGISTER_SCRIPT = "redis.call('set', KEYS[1], ARGV[1]) " +
