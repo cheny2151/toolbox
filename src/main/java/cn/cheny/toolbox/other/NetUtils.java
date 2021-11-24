43624eea7ae710827a455998b7d410bfe932e12a
chenyi5 <chenyi5@joyy.com>
2021-11-24 17:18:40 +0800

    update NetUtils

--- src/main/java/cn/cheny/toolbox/other/NetUtils.java
+++ src/main/java/cn/cheny/toolbox/other/NetUtils.java
@@ -14,6 +14,8 @@ public class NetUtils {
 
     private final static Pattern LOCAL_IP_PATTERN = Pattern.compile("127(\\.\\d{1,3}){3}$");
 
+    private final static Pattern IP_PATTERN = Pattern.compile("(2(5[0-5]|[0-4]\\d))|[0-1]?\\d{1,2}");
+
     public static String getHost() {
         try {
             return InetAddress.getLocalHost().getHostAddress();
@@ -26,4 +28,19 @@ public class NetUtils {
         return host != null && (LOCAL_IP_PATTERN.matcher(host).matches() || host.equalsIgnoreCase("localhost"));
     }
 
+    public static boolean isIp(String ip) {
+        return ip != null && (IP_PATTERN.matcher(ip).matches() || ip.equalsIgnoreCase("localhost"));
+    }
+
+    public static IpAndPort parse(String text) {
+        String[] split = text.split(":");
+        if (split.length != 2) {
+            throw new IllegalArgumentException(text + " is not a host and port");
+        }
+        String ip = split[0];
+        if (!isIp(ip)) {
+            throw new IllegalArgumentException(ip + " is not a ip");
+        }
+        return new IpAndPort(ip, Integer.parseInt(split[1]));
+    }
 }
