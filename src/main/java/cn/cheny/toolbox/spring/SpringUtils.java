58801cc1e199c8205b4ffefff4ef7199e13c02ff
chenyi5 <chenyi5@joyy.com>
2023-02-02 10:55:06 +0800

    SpringUtils增加registry方法

--- src/main/java/cn/cheny/toolbox/spring/SpringUtils.java
+++ src/main/java/cn/cheny/toolbox/spring/SpringUtils.java
@@ -1,5 +1,6 @@
 package cn.cheny.toolbox.spring;
 
+import cn.cheny.toolbox.exception.ToolboxRuntimeException;
 import cn.cheny.toolbox.other.parsing.GenericTokenParser;
 import cn.cheny.toolbox.reflect.TypeUtils;
 import cn.cheny.toolbox.scan.PathImplementationClassBuilder;
@@ -7,8 +8,12 @@ import cn.cheny.toolbox.scan.ScanException;
 import cn.cheny.toolbox.spring.properties.ToolboxDefaultProperties;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.Environment;
 import org.springframework.expression.Expression;
 import org.springframework.expression.ExpressionParser;
@@ -39,7 +44,9 @@ public class SpringUtils implements ApplicationContextAware {
 
     private static final StandardEvaluationContext evaluationContext = new StandardEvaluationContext();
 
-    private static ApplicationContext applicationContext;
+    private static ConfigurableApplicationContext applicationContext;
+
+    private static DefaultListableBeanFactory beanFactory;
 
     private static Environment env;
 
@@ -54,9 +61,11 @@ public class SpringUtils implements ApplicationContextAware {
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        SpringUtils.applicationContext = applicationContext;
+        ConfigurableApplicationContext configurableApplicationContext = (ConfigurableApplicationContext) applicationContext;
+        SpringUtils.applicationContext = configurableApplicationContext;
         SpringUtils.env = applicationContext.getEnvironment();
         SpringUtils.inSpring = true;
+        SpringUtils.beanFactory = (DefaultListableBeanFactory) configurableApplicationContext.getBeanFactory();
         evaluationContext.setBeanResolver((context, beanName) -> applicationContext.getBean(beanName));
         new Thread(() -> {
             SpringUtilsAware.defaultAware().forEach(aware -> aware.after(toolboxDefaultProperties));
@@ -98,6 +107,27 @@ public class SpringUtils implements ApplicationContextAware {
     public static <T> Collection<T> getBeansOfType(Class<T> tClass) {
         checkInSpring();
         return applicationContext.getBeansOfType(tClass).values();
+    }
+
+    public static Object registerBean(String beanName, BeanDefinitionBuilder builder) {
+        checkInSpring();
+        AbstractBeanDefinition definition = builder.getRawBeanDefinition();
+        if (beanFactory.containsBean(beanName)) {
+            throw new ToolboxRuntimeException(String.format("Bean:'%s'已注册，若需覆盖请强制执行", beanName));
+        }
+        beanFactory.registerBeanDefinition(beanName, definition);
+        return applicationContext.getBean(beanName);
+    }
+
+    public static Object forceRegisterBean(String beanName, BeanDefinitionBuilder builder) {
+        checkInSpring();
+        AbstractBeanDefinition definition = builder.getRawBeanDefinition();
+        if (beanFactory.containsBean(beanName)) {
+            log.info("Remove old BeanDefinition '{}' to register new", beanName);
+            beanFactory.removeBeanDefinition(beanName);
+        }
+        beanFactory.registerBeanDefinition(beanName, definition);
+        return applicationContext.getBean(beanName);
     }
 
     public static Environment getEnvironment() {
